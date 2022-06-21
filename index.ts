127143bbf4c10d031086d7666fae57f67198c47b
Rishav Bhowmik <rishav@imagekit.io>
2022-06-21 17:38:34 +0530

    WebhookSignature - sign, verify & parse webhook request - WebhookSignature class - WebhookEven types (all video.transmation events) - Tests for Webhook signature

--- index.ts
+++ index.ts
@@ -39,6 +39,7 @@ import {
 import { IKCallback } from "./libs/interfaces/IKCallback";
 import manage from "./libs/manage";
 import signature from "./libs/signature";
+import WebhookSignature, { WebhookSignatureError, makePayloadTimestampSha1Hash, serializeSignature, deserializeSignature } from "./utils/webhook-signature";
 import upload from "./libs/upload";
 import customMetadataField from "./libs/manage/custom-metadata-field";
 /*
@@ -76,7 +77,15 @@ const promisify = function <T = void>(thisContext: ImageKit, fn: Function) {
   };
 };
 
-class ImageKit {
+class ImageKitStaticUtils {
+  static WebhookSignature = WebhookSignature;
+  static WebhookSignatureError = WebhookSignatureError;
+  static makePayloadTimestampSha1Hash = makePayloadTimestampSha1Hash;
+  static serializeWebhookSignature = serializeSignature;
+  static deserializeWebhookSignature = deserializeSignature;
+}
+
+class ImageKit extends ImageKitStaticUtils {
   options: ImageKitOptions = {
     uploadEndpoint: "https://upload.imagekit.io/api/v1/files/upload",
     publicKey: "",
@@ -86,6 +95,7 @@ class ImageKit {
   };
 
   constructor(opts: ImageKitOptions = {} as ImageKitOptions) {
+    super();
     this.options = _.extend(this.options, opts);
     if (!this.options.publicKey) {
       throw new Error(errorMessages.MANDATORY_PUBLIC_KEY_MISSING.message);
