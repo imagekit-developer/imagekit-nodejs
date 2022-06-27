ff68298286ea5e6f51fba14fb93643a839195c9a
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2022-06-27 15:45:00 +0530

    Updated types for CustomMetadataField

--- libs/interfaces/CustomMetatadaField.ts
+++ libs/interfaces/CustomMetatadaField.ts
@@ -1,23 +1,67 @@
-export interface CustomMetadataFieldSchema {
-    type: string;
-    selectOptions?: (string | number | boolean)[];
-    defaultValue?: string | number | boolean | (string | number | boolean)[];
-    isValueRequired?: boolean;
-    minValue?: string | number;
-    maxValue?: string | number;
-    minLength?: number;
-    maxLength?: number;
-}
+type RequiredSchema<T> = {
+    isValueRequired: true;
+    defaultValue: T;
+} | {
+    isValueRequired?: false;
+    defaultValue?: T;
+};
 
-export interface CustomMetadataFieldSchemaMinusType {
-    selectOptions?: (string | number | boolean)[];
-    defaultValue?: string | number | boolean | (string | number | boolean)[];
-    isValueRequired?: boolean;
-    minValue?: string | number;
-    maxValue?: string | number;
+type CustomMetadataTextField = RequiredSchema<string> & {
+    type: "Text";
     minLength?: number;
     maxLength?: number;
-}
+};
+
+type CustomMetadataTextareaField = RequiredSchema<string> & {
+    type: "Textarea";
+    minLength?: number;
+    maxLength?: number;
+};
+
+type CustomMetadataNumberField = RequiredSchema<number> & {
+    type: "Number";
+    minValue?: string | number;
+    maxValue?: string | number;
+};
+
+type CustomMetadataDateField = RequiredSchema<number> & {
+    type: "Date";
+    minValue?: string | number;
+    maxValue?: string | number;
+};
+
+type CustomMetadataBooleanField = RequiredSchema<Boolean> & {
+    type: "Boolean";
+};
+
+type CustomMetadataSingleSelectField = RequiredSchema<Array<string | boolean | number>> & {
+    type: "SingleSelect";
+    selectOptions: Array<string | boolean | number>;
+};
+
+type CustomMetadataMultiSelectField = RequiredSchema<Array<string | boolean | number>> & {
+    type: "MultiSelect";
+    selectOptions: Array<string | boolean | number>;
+};
+
+export type CustomMetadataFieldSchema =
+    | CustomMetadataTextField
+    | CustomMetadataTextareaField
+    | CustomMetadataNumberField
+    | CustomMetadataDateField
+    | CustomMetadataBooleanField
+    | CustomMetadataSingleSelectField
+    | CustomMetadataMultiSelectField;
+
+export type CustomMetadataFieldSchemaMinusType =
+    | Omit<CustomMetadataTextField, "type">
+    | Omit<CustomMetadataTextareaField, "type">
+    | Omit<CustomMetadataNumberField, "type">
+    | Omit<CustomMetadataDateField, "type">
+    | Omit<CustomMetadataBooleanField, "type">
+    | Omit<CustomMetadataSingleSelectField, "type">
+    | Omit<CustomMetadataMultiSelectField, "type">;
+
 /**
  * Create a new custom metadata field
  *
