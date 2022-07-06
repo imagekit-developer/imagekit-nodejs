fdfa8daed7366343e4a1678b3fc384bd968cc139
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2022-07-06 12:11:39 +0530

    Supported Transforms updated types

--- libs/interfaces/Transformation.ts
+++ libs/interfaces/Transformation.ts
@@ -1,10 +1,10 @@
-import supportedTransforms from "../constants/supportedTransforms";
+import { SupportedTransformsType } from "../constants/supportedTransforms";
 
 export type TransformationPosition = "path" | "query";
 
 export type Transformation = Partial<
   | {
-      [key in keyof typeof supportedTransforms]: string | boolean | number;
+      [key in SupportedTransformsType]: string | boolean | number;
     }
-  | { [key: string]: string | string | boolean | number }
+  | { [key: string]: string | boolean | number }
 >;
