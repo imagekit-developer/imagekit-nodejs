04aac2d4a32273d3509a7a58dbacf3fe958901b3
Manu Chaudhary <manu@imagekit.io>
2022-07-06 11:09:23 +0530

    try

--- test-e2e.sh
+++ test-e2e.sh
@@ -1,9 +1,12 @@
+rm -rf .cache
+export YARN_CACHE_FOLDER=.cache
 npm pack
 cd tests/e2e/node-js
 yarn init --yes
 echo "Installing local bundle from TAR in NodeJS project"
 yarn add ../../../imagekit*.tgz
 node index.js;test_result=$?
+echo test_result
 if ((test_result != 0)); then
   printf '%s\n' "Final bundle not working in NodeJS project" >&2
   exit 1                                
@@ -16,6 +19,7 @@ yarn add typescript --dev
 echo "Installing local bundle from TAR in Typescript project"
 yarn add ../../../imagekit*.tgz
 npx tsc && node index.js;test_result=$?
+echo test_result
 if ((test_result != 0)); then
   printf '%s\n' "Final bundle not working in Typescript project" >&2
   exit 1                                
